7145aa4017596fa218fbcb99173fd80adfc56562
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2024-05-08 22:27:17 +0200

    Uploaded my belgian assignment

---
+++ belgium/actions/byr.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/stylesheet.css">
+    <title>belgium-strongbox.gov</title>
+</head>
+<body>
+    <?php
+    $sentence = $_POST['sentence'];
+    $colors = ['black', 'yellow', 'red'];
+    $wordsAmount = str_word_count($sentence);
+
+    if ($wordsAmount < 10) {
+        $msg = "$wordsAmount words isn't enough !";
+        header('Location: ../index.php?msg=' . $msg);
+    }
+    else {
+        $lst = explode(' ', $sentence);
+        $counter = 0;
+        foreach ($lst as $index => $word) {
+            foreach (str_split($word) as $char) {
+                echo "<span id='" . $colors[$counter % 3] . "'>" . $char . "</span>";
+                $counter++;
+            }
+            echo ' ';
+        }
+    }
+    ?>
+    <br>
+    <hr>
+    <img id="tintin" src="../images/tintin.gif" alt="Tintin's victory dance">
+</body>
+</html>
---
+++ belgium/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/stylesheet.css">
+    <title>belgium.gov</title>
+</head>
+<body>
+    <header>
+        <a href="https://github.com/nml18"><img src="images/signature.png" alt="Nataniel's signature"></a>
+        <p id="title">Belgian assignment</p>
+        <div></div>
+    </header>
+    <form action="actions/byr.php" method="post">
+        <label>Type at least 10 cool words here :</label>
+        <input type="text" name="sentence">
+        <input type="submit" name="send">
+    </form>
+    <?php
+    if (isset($_GET['msg'])) {
+        echo "<p id='warning'>" . $_GET['msg'] . "</p>";
+    }
+    ?>
+    <img src="images/smurfs.gif" alt="Unhappy smurf">
+</body>
+</html>
