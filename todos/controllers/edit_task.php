0e968a455546ca736077943547e0a1a1c117a246
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2024-06-17 23:06:32 +0200

    Uploaded todos Ver.1.1

--- todos/controllers/edit_task.php
+++ todos/controllers/edit_task.php
@@ -4,11 +4,17 @@ require '../system/functions.php';
 $file_path = '../models/tasks.csv';
 $tasks = getTasks($file_path);
 
-# Edit the task
-$tasks[$_POST['id']]['task'] = $_POST['task'];
-
-# Update the task database
-saveTasks($file_path, $tasks);
+# Check if the task is empty or not
+if(empty($_POST['task'])){
+    $_SESSION['error'] = true;
+}
+else{
+    $_SESSION['error'] = false;
+    # Update the task database with the edited task
+    $tasks[$_POST['id']]['task'] = $_POST['task'];
+    # Update the task database
+    saveTasks($file_path, $tasks);
+}
 
 # Jump back to the main page
-header('Location: ../index.php');
+header('Location: ../index.php?page=edit_task');
--- todos/views/edit_task.php
+++ todos/views/edit_task.php
@@ -1,7 +1,23 @@
+<?php
+session_start();
+if(isset($_SESSION['error'])){
+    if($_SESSION['error']){
+        $result = 'Failed to edit the task.';
+        $color = 'pico-color-red-250';
+    }
+    else{
+        $result = 'Successfully edited the task.';
+        $color = 'pico-color-jade-250';
+    }
+}
+unset($_SESSION['error']);
+?>
+
 <form action="./controllers/edit_task.php" method="post">
     <label>Edit your task here :</label>
     <input type="hidden" name="id" value="<?php echo $_POST['id']?>">
     <input type="text" name="task">
     <input type="submit">
+    <span class=<?php echo $color?>><?php echo $result?></span>
 </form>
 <li><a href="index.php">Home</a></li>
