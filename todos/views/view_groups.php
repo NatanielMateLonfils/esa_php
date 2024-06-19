b116981c768663b85ac80bf7157b65c1e7e54096
Nataniel Maté Lonfils <nataniel.lonfils.esa@gmail.com>
2024-06-19 23:44:43 +0200

    Uploaded todos Ver.1.4.6

--- todos/views/view_groups.php
+++ todos/views/view_groups.php
@@ -1,7 +1,7 @@
-<?php foreach ($groups as $group_id => $group): ?>
+<?php foreach ($groups as $group_id => $group_title): ?>
     <div class="group">
         <div class="group_title">
-            <h2 class="pico-color-purple-450"><?php echo $group['group']?></h2>
+            <h2 class="pico-color-purple-450"><?php echo $group_title['group']?></h2>
         </div>
         <div class="group_options">
             <form action="index.php?page=edit_group" method="post">
@@ -10,62 +10,62 @@
             </form>
             <form action="./controllers/delete_group.php" method="post">
                 <input type="hidden" name="group_id" value="<?php echo $group_id?>">
-                <button type="submit">Delete</button>
+                <button type="submit" class="delete_button">Delete</button>
             </form>
         </div>
     </div>
     <?php foreach($tasks as $group_name => $group): ?>
-        <?php if(!($group_name == 'Ungrouped')): ?>
-            <?php foreach($group as $task_id => $task): ?>
-                <div class="task">
-                    <div class="task_title">
-                        <li class="column" id=<?php echo $tasks[$group_name][$task_id]['completed']?>><?php echo $tasks[$group_name][$task_id]['task']?></li>
-                    </div>
-                    <div class="task_options">
-                        <?php if(!($task_id == 0)): ?>
-                            <form class="column" action="./controllers/swing_task.php" method="post">
-                                <input type="hidden" name="group" value="<?php echo $group_name?>">
-                                <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                                <input type="hidden" name="direction" value="up">
-                                <button type="submit" id="arrow">&#8593;</button>
-                            </form>
-                        <?php endif; ?>
-                        <?php if(!(count($tasks[$group_name])-1 == $task_id)): ?>
-                            <form class="column" action="./controllers/swing_task.php" method="post">
-                                <input type="hidden" name="group" value="<?php echo $group_name?>">
-                                <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                                <input type="hidden" name="direction" value="down">
-                                <button type="submit" id="arrow">&#8595;</button>
-                            </form>
-                        <?php endif; ?>
-                        <form class="column" action="./controllers/mark_task.php" method="post">
-                            <input type="hidden" name="group" value="<?php echo $group_name?>">
-                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                            <button type="submit">Mark</button>
-                        </form>
-                        <form class="column" action="index.php?page=edit_task" method="post">
-                            <input type="hidden" name="group" value="<?php echo $group_name?>">
-                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                            <button type="submit">Edit</button>
-                        </form>
-                        <form class="column" action="index.php?page=move_task" method="post">
-                            <input type="hidden" name="group" value="<?php echo $group_name?>">
-                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                            <button type="submit">Move</button>
-                        </form>
-                        <form class="column" action="./controllers/duplicate_task.php" method="post">
-                            <input type="hidden" name="group" value="<?php echo $group_name?>">
-                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                            <button type="submit">Duplicate</button>
-                        </form>
-                        <form class="column" action="./controllers/delete_task.php" method="post">
-                            <input type="hidden" name="group" value="<?php echo $group_name?>">
-                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
-                            <button type="submit" class="delete_button">Delete</button>
-                        </form>
-                    </div>
+        <?php foreach($group as $task_id => $task): ?>
+            <?php if ($task['group'] == $group_title['group']): ?>
+            <div class="task">
+                <div class="task_title">
+                    <li class="column" id=<?php echo $tasks[$group_name][$task_id]['completed']?>><?php echo $tasks[$group_name][$task_id]['task']?></li>
                 </div>
-            <?php endforeach; ?>
-        <?php endif; ?>
+                <div class="task_options">
+                    <?php if(!($task_id == 0)): ?>
+                        <form class="column" action="./controllers/swing_task.php" method="post">
+                            <input type="hidden" name="group" value="<?php echo $group_name?>">
+                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                            <input type="hidden" name="direction" value="up">
+                            <button type="submit" id="arrow">&#8593;</button>
+                        </form>
+                    <?php endif; ?>
+                    <?php if(!(count($tasks[$group_name])-1 == $task_id)): ?>
+                        <form class="column" action="./controllers/swing_task.php" method="post">
+                            <input type="hidden" name="group" value="<?php echo $group_name?>">
+                            <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                            <input type="hidden" name="direction" value="down">
+                            <button type="submit" id="arrow">&#8595;</button>
+                        </form>
+                    <?php endif; ?>
+                    <form class="column" action="./controllers/mark_task.php" method="post">
+                        <input type="hidden" name="group" value="<?php echo $group_name?>">
+                        <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                        <button type="submit">Mark</button>
+                    </form>
+                    <form class="column" action="index.php?page=edit_task" method="post">
+                        <input type="hidden" name="group" value="<?php echo $group_name?>">
+                        <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                        <button type="submit">Edit</button>
+                    </form>
+                    <form class="column" action="index.php?page=move_task" method="post">
+                        <input type="hidden" name="group" value="<?php echo $group_name?>">
+                        <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                        <button type="submit">Move</button>
+                    </form>
+                    <form class="column" action="./controllers/duplicate_task.php" method="post">
+                        <input type="hidden" name="group" value="<?php echo $group_name?>">
+                        <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                        <button type="submit">Duplicate</button>
+                    </form>
+                    <form class="column" action="./controllers/delete_task.php" method="post">
+                        <input type="hidden" name="group" value="<?php echo $group_name?>">
+                        <input type="hidden" name="task_id" value="<?php echo $task_id?>">
+                        <button type="submit" class="delete_button">Delete</button>
+                    </form>
+                </div>
+            </div>
+            <?php endif; ?>
+        <?php endforeach; ?>
     <?php endforeach; ?>
 <?php endforeach; ?>
